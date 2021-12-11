6e9fb9cc7363b498649f078393f513bc13788699
Menescal <lexdmm@protonmail.com>
2021-12-10 23:45:31 -0300

    some settings adjusts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
+
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { GraphQLModule } from '@nestjs/graphql'
-import { join } from 'path/posix'
+import { join } from 'path'
 import { UserModule } from './user/user.module'
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(),
         GraphQLModule.forRoot({
-            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql')
         }),
-        UserModule,
+        UserModule
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [AppController]
 })
-export class AppModule { }
+export class AppModule {}
