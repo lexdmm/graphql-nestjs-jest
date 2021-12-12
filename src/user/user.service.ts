c42a31c5f5774c5f8f72303e88b1aa0aa3846239
Menescal <lexdmm@protonmail.com>
2021-12-11 23:02:45 -0300

    development update and delete mutations

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { CreateUserInput } from './dto/create-user.input'
+import { UpdateUserInput } from './dto/update-user.input'
 import { User } from './user.entity'
 
 @Injectable()
@@ -9,22 +10,48 @@ export class UserService {
     constructor(
         @InjectRepository(User) //Ta dizendo que o papel de criar a instancia é do repositório
         private _userRepository: Repository<User>
-    ) {}
+    ) { }
 
     async findAllUsers(): Promise<User[]> {
         const users = await this._userRepository.find()
         return users
     }
 
+    async findUserById(id: string): Promise<User> {
+        const user = await this._userRepository.findOne(id)
+        if (!user) {
+            throw new InternalServerErrorException('User not found')
+        }
+        return user
+    }
+
+    async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+        const user = await this.findUserById(id)
+        await this._userRepository.update(user, { ...data })
+
+        const userUpdated = this._userRepository.create({ ...user, ...data })
+        return userUpdated
+    }
+
     async createUser(data: CreateUserInput): Promise<User> {
-        const user = await this._userRepository.create(data)
+        const user = this._userRepository.create(data)
         const userSaved = await this._userRepository.save(user)
 
         if (!userSaved) {
             throw new InternalServerErrorException(
-                'Ocorreu um erro ao criar o usuário.'
+                'There was an error creating the user'
             )
         }
         return userSaved
+    }
+
+    async deleteUser(id: string): Promise<boolean> {
+        const user = await this.findUserById(id)
+        const deleted = await this._userRepository.delete(user)
+
+        if (deleted) {
+            return true
+        }
+        return false
     }
 }
